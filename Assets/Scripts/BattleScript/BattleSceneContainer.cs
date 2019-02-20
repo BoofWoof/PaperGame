3a847d077eae59ca7f4652bd64b489f661bf8002
Garrett <gfosdick@email.arizona.edu>
2019-02-20 12:45:22 -0700

    Combat Scene
    Combat scenes are now generated and enemies and allies are placed.

---
+++ Assets/Scripts/BattleScript/BattleSceneContainer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneContainer : MonoBehaviour
+{
+    public GameObject PlayerPosition1;
+    public GameObject PlayerPosition2;
+    public GameObject EnemyPosition1 = null;
+    public GameObject EnemyPosition2 = null;
+    public GameObject EnemyPosition3 = null;
+    public GameObject EnemyPosition4 = null;
+    public GameObject EnemyPosition5 = null;
+    public GameObject EnemyPosition6 = null;
+    [HideInInspector] public List<GameObject> PlayerPositions = new List<GameObject>();
+    [HideInInspector] public List<GameObject> EnemyPositions = new List<GameObject>();
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        PlayerPositions.Add(PlayerPosition1);
+        PlayerPositions.Add(PlayerPosition2);
+        if (EnemyPosition1 != null)
+        {
+            EnemyPositions.Add(EnemyPosition1);
+        }
+        if (EnemyPosition2 != null)
+        {
+            EnemyPositions.Add(EnemyPosition2);
+        }
+        if (EnemyPosition3 != null)
+        {
+            EnemyPositions.Add(EnemyPosition3);
+        }
+        if (EnemyPosition4 != null)
+        {
+            EnemyPositions.Add(EnemyPosition4);
+        }
+        if (EnemyPosition5 != null)
+        {
+            EnemyPositions.Add(EnemyPosition5);
+        }
+        if (EnemyPosition6 != null)
+        {
+            EnemyPositions.Add(EnemyPosition6);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BattleScript/CombatController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatController : MonoBehaviour
+{
+    //SceneConstructers
+    public GameObject PlayerInput;
+    public GameObject PartnerInput;
+    public GameObject Enemy_Select;
+    public GameObject Scene_Select;
+    public GameObject GUI_Select;
+    //Constant Inputs
+    public GameObject MenuObject;
+    //Instantiated Scene Objects
+    private List<GameObject> playerPositions;
+    private List<GameObject> enemyPositions;
+    private List<GameObject> enemyObjectList;
+    private GameObject GUI;
+
+    //Instantiated Scene Objects
+    private GameObject scene;
+    private List<GameObject> enemyList;
+    private GameObject Player;
+    private GameObject Partner;
+
+    //GuiTest
+    private GameObject tempScoreGuiElement;
+    private int tempScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject CombatMenu = Instantiate<GameObject>(MenuObject, new Vector3(transform.position.x-4, transform.position.y-1, transform.position.z), Quaternion.identity);
+        CombatMenu.GetComponent<BattleMenu>().comControl = gameObject;
+        //Create Scene
+        scene = (GameObject)Instantiate(Scene_Select, transform.position, Quaternion.identity);
+        playerPositions = scene.GetComponent<BattleSceneContainer>().PlayerPositions;
+        enemyPositions = scene.GetComponent<BattleSceneContainer>().EnemyPositions;
+        //Load Enemies
+        enemyObjectList = (List<GameObject>)Enemy_Select.GetComponent<EnemyContainer>().EnemyList;
+        for (int i = 0; i < enemyObjectList.Count; i++)
+        {
+            Instantiate<GameObject>(enemyObjectList[i], enemyPositions[i].transform.position, Quaternion.identity);
+        }
+        Player = Instantiate<GameObject>(PlayerInput, playerPositions[0].transform.position, Quaternion.identity);
+        Partner = Instantiate<GameObject>(PartnerInput, playerPositions[1].transform.position, Quaternion.identity);
+        GUI = Instantiate<GameObject>(GUI_Select, Vector3.zero, Quaternion.identity);
+        tempScoreGuiElement = GUI.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            tempScore++;
+        }
+        tempScoreGuiElement.GetComponent<UnityEngine.UI.Text>().text = "Score: " + tempScore;
+    }
+}
