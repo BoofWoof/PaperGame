6a8e6535a6bc631210f4c9e9925e89961543080a
Garrett <gfosdick@email.arizona.edu>
2019-03-03 23:32:29 -0700

    PartnerAdded
    BigBoof Partner Added

---
+++ Assets/PreFab/Characters/PlayerControlledCharacters/Werewolf/Big/SwipeAttack/SwipeAttackCutscene.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeAttackCutscene : CutSceneClass
+{
+    public int amount;
+    public FighterClass.attackType type;
+    public FighterClass.statusEffects effects;
+    public FighterClass.attackLocation location;
+    public GameObject source;
+    public GameObject damageTarget;
+
+    private int attackPhase = 0;
+    private float timeCount = 0;
+    private bool attemptMade = false;
+
+    private Vector3 positionDistance;
+    private Vector3 positionHome;
+    private float speed = 12.0f;
+
+    private void Start()
+    {
+        positionHome = transform.parent.GetComponent<FighterClass>().HomePosition;
+        positionDistance = damageTarget.transform.position - transform.parent.position;
+    }
+
+    private void Update()
+    {
+        if (attackPhase == 0)
+        {
+            if (transform.parent.GetComponent<Animator>() != null)
+            {
+                transform.parent.GetComponent<Animator>().SetTrigger("AttackSlicePrep");
+            }
+            attackPhase++;
+        }
+        if (attackPhase == 1)
+        {
+            timeCount = timeCount + Time.deltaTime;
+            if (Input.GetButtonUp("Fire1") && attemptMade == false)
+            {
+                attemptMade = true;
+                if (timeCount > 1.75)
+                {
+                    amount = amount * 2;
+                }
+                if (transform.parent.GetComponent<Animator>() != null)
+                {
+                    print("hi");
+                    transform.parent.GetComponent<Animator>().SetTrigger("AttackSlice");
+                }
+                attackPhase++;
+            }
+            if (timeCount >= 3)
+            {
+                if (transform.parent.GetComponent<Animator>() != null)
+                {
+                    print("hi");
+                    transform.parent.GetComponent<Animator>().SetTrigger("AttackSlice");
+                }
+                attackPhase++;
+            }
+        }
+        else if (attackPhase == 2)
+        {
+            transform.parent.position = Vector3.MoveTowards(transform.parent.position, damageTarget.transform.position, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.parent.position, damageTarget.transform.position) < speed * Time.deltaTime)
+            {
+                transform.parent.position = damageTarget.transform.position;
+                attackPhase++;
+            }
+        }
+        else if (attackPhase == 3)
+        {
+            damageTarget.GetComponent<FighterClass>().attackEffect(amount, type, effects, location, source);
+            attackPhase++;
+        }
+        else if (attackPhase == 4)
+        {
+            transform.parent.position = Vector3.MoveTowards(transform.parent.position, transform.parent.position+positionDistance, speed * Time.deltaTime);
+            if (transform.parent.position.x > 10)
+            {
+                transform.parent.position = new Vector3(-10, positionHome.y, positionHome.z);
+                attackPhase++;
+            }
+        }
+        else if (attackPhase == 5)
+        {
+            transform.parent.position = Vector3.MoveTowards(transform.parent.position, positionHome, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.parent.position, positionHome) < speed * Time.deltaTime)
+            {
+                if (transform.parent.GetComponent<Animator>() != null)
+                {
+                    transform.parent.GetComponent<Animator>().SetTrigger("Stop");
+                }
+                transform.parent.position = positionHome;
+                attackPhase++;
+            }
+        }
+        else if (attackPhase == 6)
+        {
+            cutsceneDone();
+        }
+    }
+}
---
+++ Assets/PreFab/Characters/PlayerControlledCharacters/Werewolf/Big/SwipeAttack/SwipeAttackMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeAttackMove : MoveClass
+{
+    public override void effect()
+    {
+        GameObject Attack = new GameObject();
+        SwipeAttackCutscene A = Attack.AddComponent<SwipeAttackCutscene>();
+        A.amount = power;
+        A.damageTarget = enemyList[targetID].CharacterObject;
+        A.effects = FighterClass.statusEffects.None;
+        A.location = FighterClass.attackLocation.All;
+        A.source = friendlyList[sourceID].CharacterObject;
+        A.type = FighterClass.attackType.Normal;
+        CombatController.addCutseenEvent(Attack, friendlyList[sourceID].CharacterObject, true);
+    }
+}
