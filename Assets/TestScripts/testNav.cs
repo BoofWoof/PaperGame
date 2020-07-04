16016cf63a9ca6a6702b561849ad6258d98ef307
Garrett <gfosdick@email.arizona.edu>
2020-07-04 14:51:19 -0700

    Enemies can now roam.
    Still need to make it so they can't push the character around.

---
+++ Assets/TestScripts/testNav.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class testNav : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public float walkRadius;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OverworldController.gameMode != OverworldController.gameModeOptions.Cutscene)
+        {
+            if (agent.remainingDistance < 0.1 * walkRadius){
+                Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+                randomDirection += transform.position;
+                NavMeshHit hit;
+                NavMesh.SamplePosition(randomDirection, out hit, walkRadius, NavMesh.AllAreas);
+                agent.SetDestination(hit.position);
+                }
+        }
+        transform.rotation = Quaternion.identity;
+    }
+}
