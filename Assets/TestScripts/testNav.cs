42dcc6b2ed12ce42f2ab4d1a98b44f762b95afe5
Garrett <gfosdick@email.arizona.edu>
2020-07-04 17:00:32 -0700

    Added pauses to movement.

--- Assets/TestScripts/testNav.cs
+++ Assets/TestScripts/testNav.cs
@@ -7,19 +7,31 @@ public class testNav : MonoBehaviour
 {
     public NavMeshAgent agent;
     public float walkRadius;
+    public float secondsTillMove = 2;
+    private float wait_count = 0;
 
     // Update is called once per frame
     void Update()
     {
         if (OverworldController.gameMode != OverworldController.gameModeOptions.Cutscene)
         {
-            if (agent.remainingDistance < 0.1 * walkRadius){
-                Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
-                randomDirection += transform.position;
-                NavMeshHit hit;
-                NavMesh.SamplePosition(randomDirection, out hit, walkRadius, NavMesh.AllAreas);
-                agent.SetDestination(hit.position);
+            if (agent.remainingDistance < 0.01 * walkRadius)
+            {
+                wait_count += Time.deltaTime;
+                if (wait_count >= secondsTillMove)
+                {
+                    wait_count = 0;
+                    Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+                    randomDirection += transform.position;
+                    NavMeshHit hit;
+                    NavMesh.SamplePosition(randomDirection, out hit, walkRadius, NavMesh.AllAreas);
+                    agent.SetDestination(hit.position);
                 }
+            }
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
         }
         transform.rotation = Quaternion.identity;
     }
