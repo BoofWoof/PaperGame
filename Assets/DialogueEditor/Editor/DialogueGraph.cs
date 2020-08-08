bdfee265624eddab59f81e8b9a4d0ca2cbbbba98
Garrett <gfosdick@email.arizona.edu>
2020-08-08 10:45:22 -0700

    Further Dialogue Graph Development
    Added saving and variable blackboard.

---
+++ Assets/DialogueEditor/Editor/DialogueGraph.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using System;
+using UnityEditor.Experimental.GraphView;
+using System.Linq;
+
+public class DialogueGraph : EditorWindow
+{
+    private DialogueGraphView _graphView;
+    private string _filename = "New Narrative";
+
+    [MenuItem("Graph/Dialogue Graph")]
+    public static void OpenDialogueGraphWindow()
+    {
+        var window = GetWindow<DialogueGraph>();
+        window.titleContent = new GUIContent("Dialogue Graph");
+    }
+
+    private void ConstructGraphView()
+    {
+        _graphView = new DialogueGraphView(this)
+        {
+            name = "Dialogue Graph"
+        };
+        _graphView.StretchToParentSize();
+        rootVisualElement.Add(_graphView);
+    }
+
+    private void GenerateToolbar()
+    {
+        var toolbar = new Toolbar();
+
+        var fileNameTextField = new TextField(label: "FileName");
+        fileNameTextField.SetValueWithoutNotify(_filename);
+        fileNameTextField.MarkDirtyRepaint();
+        fileNameTextField.RegisterValueChangedCallback(evt => _filename = evt.newValue);
+        toolbar.Add(fileNameTextField);
+
+        toolbar.Add(new Button(clickEvent: () => RequestDataOperation(true)) { text = "Save Data" });
+        toolbar.Add(new Button(clickEvent: () => RequestDataOperation(false)) { text = "Load Data" });
+
+        //var nodeCreateButton = new Button(clickEvent: () =>
+        //    {
+        //        _graphView.CreateNode("Dialogue Node");
+        //    }
+        //);
+        //nodeCreateButton.text = "Create Node";
+        //toolbar.Add(nodeCreateButton);
+
+        rootVisualElement.Add(toolbar);
+    }
+
+    private void RequestDataOperation(bool save)
+    {
+        if (string.IsNullOrEmpty(_filename))
+        {
+            EditorUtility.DisplayDialog("Invalid file name!", "Please enter a valid file name.", "OK");
+        }
+
+        var saveUtility = GraphSaveUtility.GetInstance(_graphView);
+        if (save)
+        {
+            saveUtility.SaveGraph(_filename);
+        } else
+        {
+            saveUtility.LoadGraph(_filename);
+        }
+    }
+
+    private void GenerateMiniMap()
+    {
+        var miniMap = new MiniMap { anchored = true };
+        var cords = _graphView.contentViewContainer.WorldToLocal(new Vector2(this.maxSize.x-10, 30));
+        miniMap.SetPosition(new Rect(cords.x, cords.y, 200, 140));
+        _graphView.Add(miniMap);
+    }
+
+    private void OnEnable()
+    {
+        ConstructGraphView();
+        GenerateToolbar();
+        GenerateMiniMap();
+        GenerateBlackBoard();
+    }
+
+    private void GenerateBlackBoard()
+    {
+        var blackBoard = new Blackboard(_graphView);
+        blackBoard.Add(new BlackboardSection { title = "Exposed Properties" });
+        blackBoard.addItemRequested = _blackboard =>
+        {
+            _graphView.AddPropertyToBlackBoard(new ExposedProperty());
+        };
+        blackBoard.editTextRequested = (blackboard1, element, newValue) =>
+        {
+            var oldPropertyName = ((BlackboardField)element).text;
+            if(_graphView.ExposedProperties.Any(x => x.PropertyName == newValue))
+            {
+                EditorUtility.DisplayDialog("Error", "This property name already exists, please chose another one!", "OK");
+                return;
+            }
+
+            var propertyIndex = _graphView.ExposedProperties.FindIndex(x => x.PropertyName == oldPropertyName);
+            _graphView.ExposedProperties[propertyIndex].PropertyName = newValue;
+            ((BlackboardField)element).text = newValue;
+        };
+        blackBoard.SetPosition(new Rect(10, 30, 200, 300));
+        _graphView.Add(blackBoard);
+        _graphView.Blackboard = blackBoard;
+    }
+
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(_graphView);
+    }
+}
---
+++ Assets/DialogueEditor/Editor/DialogueGraphView.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
+
+public class DialogueGraphView : GraphView
+{
+    public readonly Vector2 defaultNodeSize = new Vector2(150, 200);
+
+    public Blackboard Blackboard;
+    public List<ExposedProperty> ExposedProperties = new List<ExposedProperty>();
+    private NodeSearchWindow _searchWindow;
+
+    public DialogueGraphView(EditorWindow editorWindow)
+    {
+        StyleSheet ssheet = Resources.Load<StyleSheet>(path: "DialogueGraph");
+        styleSheets.Add(styleSheet: ssheet);
+        SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+
+        var grid = new GridBackground();
+        Insert(index: 0, grid);
+        grid.StretchToParentSize();
+
+        AddElement(GenerateEntryPointNode());
+        AddSearchWindow(editorWindow);
+    }
+
+    private void AddSearchWindow(EditorWindow editorWindow)
+    {
+        _searchWindow = ScriptableObject.CreateInstance<NodeSearchWindow>();
+        _searchWindow.Init(editorWindow, this);
+        nodeCreationRequest = context => SearchWindow.Open(new SearchWindowContext(context.screenMousePosition), _searchWindow);
+    }
+
+    private Port GeneratePort(DialogueNode node, Direction portDirection, Port.Capacity capacity = Port.Capacity.Single)
+    {
+        return node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, typeof(float)); //Arbitrary Type
+    }
+
+    private DialogueNode GenerateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "START",
+            GUID = Guid.NewGuid().ToString(),
+            DialogueText = "ENTRYPOINT",
+            EntryPoint = true
+        };
+
+        var generatedPort = GeneratePort(node, Direction.Output);
+        generatedPort.portName = "Next";
+        node.outputContainer.Add(generatedPort);
+
+        node.capabilities &= ~Capabilities.Movable;
+        node.capabilities &= ~Capabilities.Deletable;
+
+        node.RefreshExpandedState();
+        node.RefreshPorts();
+
+        node.SetPosition(new Rect(100, 200, 100, 150));
+        return node;
+    }
+
+    public DialogueNode CreateDialogueNode(string nodeName, Vector2 mousePosition)
+    {
+        var dialogueNode = new DialogueNode
+        {
+            title = nodeName,
+            DialogueText = nodeName,
+            GUID = Guid.NewGuid().ToString()
+        };
+
+        var inputPort = GeneratePort(dialogueNode, Direction.Input, Port.Capacity.Multi);
+        inputPort.portName = "Input";
+        dialogueNode.inputContainer.Add(inputPort);
+
+        dialogueNode.styleSheets.Add(Resources.Load<StyleSheet>("Node"));
+
+        var button = new Button(clickEvent: () =>
+        {
+            AddChoicePort(dialogueNode);
+        });
+        button.text = "New Choice";
+        dialogueNode.titleContainer.Add(button);
+
+        var textField = new TextField(string.Empty);
+        textField.RegisterValueChangedCallback(evt =>
+        {
+            dialogueNode.DialogueText = evt.newValue;
+            //dialogueNode.title = evt.newValue;
+        });
+        textField.SetValueWithoutNotify(dialogueNode.title);
+        dialogueNode.mainContainer.Add(textField);
+
+        dialogueNode.RefreshExpandedState();
+        dialogueNode.RefreshPorts();
+        dialogueNode.SetPosition(new Rect(mousePosition, defaultNodeSize));
+
+        return dialogueNode;
+    }
+
+    public void CreateNode(string nodeName, Vector2 mousePosition)
+    {
+        AddElement(CreateDialogueNode(nodeName, mousePosition));
+    }
+
+    public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+    {
+        var compatiblePorts = new List<Port>();
+
+        ports.ForEach(funcCall: (port) =>
+        {
+            if(startPort!=port && startPort.node != port.node)
+            {
+                compatiblePorts.Add(port);
+            }
+        });
+
+        return compatiblePorts;
+    }
+
+    public void AddChoicePort(DialogueNode dialogueNode, string overriddenPortName = "")
+    {
+        var generatedPort = GeneratePort(dialogueNode, Direction.Output);
+
+        var oldLabel = generatedPort.contentContainer.Q<Label>("type");
+        generatedPort.contentContainer.Remove(oldLabel);
+
+        var outputPortCount = dialogueNode.outputContainer.Query(name: "connector").ToList().Count;
+
+        var choicePortName = string.IsNullOrEmpty(overriddenPortName)
+            ? $"Choice {outputPortCount}"
+            : overriddenPortName;
+
+        var textField = new TextField
+        {
+            name = string.Empty,
+            value = choicePortName
+        };
+        textField.RegisterValueChangedCallback(evt => generatedPort.portName = evt.newValue);
+        generatedPort.contentContainer.Add(new Label("  "));
+        generatedPort.contentContainer.Add(textField);
+        var deleteButton = new Button(() => RemovePort(dialogueNode, generatedPort))
+        {
+            text = "X"
+        };
+        generatedPort.contentContainer.Add(deleteButton);
+
+        generatedPort.portName = choicePortName;
+        dialogueNode.outputContainer.Add(generatedPort);
+        dialogueNode.RefreshExpandedState();
+        dialogueNode.RefreshPorts();
+    }
+
+    private void RemovePort(DialogueNode dialogueNode, Port generatedPort)
+    {
+        var targetEdge = edges.ToList().Where(x =>
+        x.output.portName == generatedPort.portName && x.output.node == generatedPort.node);
+
+        if (targetEdge.Any())
+        {
+            var edge = targetEdge.First();
+            edge.input.Disconnect(edge);
+            RemoveElement(targetEdge.First());
+        }
+
+        dialogueNode.outputContainer.Remove(generatedPort);
+        dialogueNode.RefreshPorts();
+        dialogueNode.RefreshExpandedState();
+    }
+
+    public void AddPropertyToBlackBoard(ExposedProperty exposedProperty)
+    {
+        var localPropertyName = exposedProperty.PropertyName;
+        var localPropertyValue = exposedProperty.PropertyValue;
+        while (ExposedProperties.Any(x => x.PropertyName == localPropertyName))
+        {
+            localPropertyName = $"{localPropertyName}(1)";
+        }
+
+        var property = new ExposedProperty();
+        property.PropertyName = localPropertyName;
+        property.PropertyValue = localPropertyValue;
+        ExposedProperties.Add(property);
+
+        var container = new VisualElement();
+        var blackboardField = new BlackboardField { text = property.PropertyName, typeText = "string property" };
+        container.Add(blackboardField);
+
+        var propertyValueTextField = new TextField("Value:")
+        {
+            value = localPropertyValue
+        };
+        propertyValueTextField.RegisterValueChangedCallback(evt =>
+        {
+            var changingPropertyIndex = ExposedProperties.FindIndex(x => x.PropertyName == property.PropertyName);
+            ExposedProperties[changingPropertyIndex].PropertyValue = evt.newValue;
+        });
+        var blackBoardValueRow = new BlackboardRow(blackboardField, propertyValueTextField);
+        container.Add(blackBoardValueRow);
+
+        Blackboard.Add(container);
+    }
+
+    public void ClearBlackBoardAndExposedProperties()
+    {
+        ExposedProperties.Clear();
+        Blackboard.Clear();
+    }
+}
