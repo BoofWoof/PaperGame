39475972237b7a011e754a0db4d7e77734dc5eeb
Garrett <gfosdick@email.arizona.edu>
2020-10-25 04:06:51 -0700

    Added Test Cutscene Navigation

---
+++ Assets/WaypointFollow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum OffMeshLinkMoveMethod
+{
+    Teleport,
+    Line,
+    Parabola
+}
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class WaypointFollow : MonoBehaviour
+{
+    public GameObject waypoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        Vector3 waypointPosition = waypoint.transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(waypointPosition, out hit, 1, NavMesh.AllAreas);
+        agent.SetDestination(hit.position);
+    }
+
+    public OffMeshLinkMoveMethod method = OffMeshLinkMoveMethod.Parabola;
+    public AnimationCurve curve = new AnimationCurve();
+    IEnumerator Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.autoTraverseOffMeshLink = false;
+        agent.updateRotation = false;
+        while (true)
+        {
+            if (agent.isOnOffMeshLink)
+            {
+                if (method == OffMeshLinkMoveMethod.Parabola)
+                    yield return StartCoroutine(Parabola(agent, 2.0f, 0.5f));
+                agent.CompleteOffMeshLink();
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator Parabola (NavMeshAgent agent, float height, float duration)
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        float normalizedTime = 0.0f;
+        while (normalizedTime < 1.0f)
+        {
+            float yOffset = height * 4.0f * (normalizedTime - normalizedTime * normalizedTime);
+            agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
+            normalizedTime += Time.deltaTime / duration;
+            yield return null;
+        }
+    }
+}
