980bcebed1ae3b6b51152ffbbddd0118f599037e
Garrett <gfosdick@email.arizona.edu>
2020-12-31 08:33:21 -0800

    Updated item menu.

--- Assets/PreFab/PauseMenu/ItemList.cs
+++ Assets/PreFab/PauseMenu/ItemList.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ItemList : MonoBehaviour
 {
+    //X Positions
+    public float initialXOffset;
     public float itemXOffset;
     public float itemXSize;
     public int visibleColumns;
 
+    //Y Positions
+    public float initialYOffset;
     public float itemYOffset;
     public float itemYSize;
     public int visibleRows;
 
-    private int currentRow = 0;
+    //Selection
+    public GameObject cursor;
+    private int topRowIdx = 0;
 
+    private int xcord = 0;
+    private int ycord = 0;
+    private float movementDelay = 0;
+
+    //Description
+    public GameObject itemDescriptions;
+    private TextMeshProUGUI descriptionText;
+
+    //Debug
     private List<int> debugItemList;
+
+    //All Items
     private List<List<GameObject>> gameObjectRow = new List<List<GameObject>>();
 
     // Start is called before the first frame update
@@ -23,22 +41,35 @@ public class ItemList : MonoBehaviour
     {
         debugItemList = new List<int>()
         {
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            0, 1, 2, 3, 3, 0, 3
         };
+
+        descriptionText = itemDescriptions.GetComponent<TextMeshProUGUI>();
+
         for (int i = 0; i < visibleRows; i++)
         {
-            int row = currentRow + i;
+            int row = topRowIdx + i;
             List<GameObject> gameObjectCol = new List<GameObject>();
             for (int j = 0; j < visibleColumns; j++)
             {
                 GameObject NewObj = new GameObject(); //Create the GameObject
                 NewObj.AddComponent<CanvasRenderer>(); //Add the Image Component script
                 Image NewImage = NewObj.AddComponent<Image>(); //Add the Image Component script
-                NewObj.transform.position = transform.position + new Vector3(itemXOffset*j, -itemYOffset*i, 0);
+                int itemIdx = row * visibleColumns + j;
+                if (itemIdx >= debugItemList.Count)
+                {
+                    NewImage.sprite = ItemMapping.defaultImage;
+                }
+                else
+                {
+                    NewImage.sprite = ItemMapping.imageMap[debugItemList[itemIdx]];
+                }
+                NewObj.transform.SetParent(transform, false);
+                NewObj.transform.position = transform.position + new Vector3(Screen.width * (itemXOffset * j - initialXOffset), Screen.height * (-itemYOffset * i - initialYOffset), 0);
                 NewObj.transform.localScale = new Vector3(itemXSize, itemYSize, 1);
-                NewObj.transform.SetParent(transform);
+                RectTransform NewObjRect = NewObj.GetComponent<RectTransform>();
+                NewObjRect.anchorMin = new Vector2(0, 0);
+                NewObjRect.anchorMax = new Vector2(1, 1);
                 gameObjectCol.Add(NewObj);
             }
             gameObjectRow.Add(gameObjectCol);
@@ -48,6 +79,86 @@ public class ItemList : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (xcord < 0)
+        {
+            xcord = 0;
+        }
+        if (xcord >= visibleColumns)
+        {
+            xcord = visibleColumns-1;
+        }
+        if (ycord < 0)
+        {
+            ycord = 0;
+        }
+        if (ycord >= visibleRows)
+        {
+            ycord = visibleRows - 1;
+        }
+        Debug.Log(ycord);
+        cursor.transform.position = transform.position + new Vector3(Screen.width * (itemXOffset * xcord - initialXOffset), Screen.height * (-itemYOffset * ycord - initialYOffset), 0);
+
+        int itemIdx = ycord * visibleColumns + xcord;
+        if(itemIdx < debugItemList.Count)
+        {
+            string itemName = ItemMapping.nameMap[debugItemList[itemIdx]];
+            string itemDescription = ItemMapping.descriptionMap[debugItemList[itemIdx]];
+
+            descriptionText.text = itemName + ": " + itemDescription;
+        } else
+        {
+            descriptionText.text = "Empty Pocket";
+        }
+
+        float xPress = Input.GetAxis("Horizontal");
+        float yPress = Input.GetAxis("Vertical");
+
+        movementDelay += Time.unscaledDeltaTime;
+        //Debug.Log(movementDelay);
+        //Debug.Log(xPress);
+        //Debug.Log(yPress);
+
+        if (movementDelay > 0.25 )
+        {
+            if (xPress > 0.5)
+            {
+                xcord += 1;
+                movementDelay = 0;
+            }
+            if (xPress < -0.5)
+            {
+                xcord -= 1;
+                movementDelay = 0;
+            }
+            if (yPress < -0.5)
+            {
+                ycord += 1;
+                movementDelay = 0;
+            }
+            if (yPress > 0.5)
+            {
+                ycord -= 1;
+                movementDelay = 0;
+            }
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            xcord += 1;
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            xcord -= 1;
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            ycord += 1;
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            ycord -= 1;
+        }
+
         /*
         foreach (List<GameObject> delrows in gameObjectRow)
         {
@@ -56,20 +167,32 @@ public class ItemList : MonoBehaviour
                 Destroy(delitems);
             }
         }
-
         gameObjectRow = new List<List<GameObject>>();
+
         for (int i = 0; i < visibleRows; i++)
         {
-            int row = currentRow + i;
+            int row = topRowIdx + i;
             List<GameObject> gameObjectCol = new List<GameObject>();
             for (int j = 0; j < visibleColumns; j++)
             {
                 GameObject NewObj = new GameObject(); //Create the GameObject
                 NewObj.AddComponent<CanvasRenderer>(); //Add the Image Component script
                 Image NewImage = NewObj.AddComponent<Image>(); //Add the Image Component script
-                NewObj.transform.position = transform.position + new Vector3(itemXOffset * j, -itemYOffset * i, 0);
+                int itemIdx1 = row * visibleColumns + j;
+                if (itemIdx1 >= debugItemList.Count)
+                {
+                    NewImage.sprite = ItemMapping.defaultImage;
+                }
+                else
+                {
+                    NewImage.sprite = ItemMapping.imageMap[debugItemList[itemIdx1]];
+                }
+                NewObj.transform.SetParent(transform, false);
+                NewObj.transform.position = transform.position + new Vector3(Screen.width * (itemXOffset * j - initialXOffset), Screen.height * (-itemYOffset * i - initialYOffset), 0);
                 NewObj.transform.localScale = new Vector3(itemXSize, itemYSize, 1);
-                NewObj.transform.SetParent(transform);
+                RectTransform NewObjRect = NewObj.GetComponent<RectTransform>();
+                NewObjRect.anchorMin = new Vector2(0, 0);
+                NewObjRect.anchorMax = new Vector2(1, 1);
                 gameObjectCol.Add(NewObj);
             }
             gameObjectRow.Add(gameObjectCol);
---
+++ Assets/PreFab/PauseMenu/ItemMapping.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemMapping : MonoBehaviour
+{
+    public static string[] nameMap;
+    public static string[] descriptionMap;
+    public static Sprite[] imageMap;
+    public static Sprite defaultImage;
+
+    public string[] inputNameMap;
+    public string[] inputDescriptionMap;
+    public Sprite[] inputImageMap;
+    public Sprite inputDefaultImage;
+
+    void Start()
+    {
+        nameMap = inputNameMap;
+        descriptionMap = inputDescriptionMap;
+        imageMap = inputImageMap;
+        defaultImage = inputDefaultImage;
+    }
+}
